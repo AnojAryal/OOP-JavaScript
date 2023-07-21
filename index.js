09c1ceddca5b0595e5e42c74045e9a40547c4b5d
FraNzY <anoj1810@gmail.com>
2023-07-21 17:31:25 +0545

    OOP in JavaScript

--- index.js
+++ index.js
@@ -27,3 +27,11 @@ const cir = createCircle(1);
 console.log(cir);
 
 //Constructor Function
+function Circle(radious){
+    this.radious = radious,
+    this.draw = function(){
+        console.log('Draw');
+    }
+}
+const another = new Circle(3);
+console.log(another);
