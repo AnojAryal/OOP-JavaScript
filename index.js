e668c065bba874fe532994d8597ae87b0a0d25e8
FraNzY <anoj1810@gmail.com>
2023-07-26 19:33:01 +0545

    OOP in JavaScript

--- index.js
+++ index.js
@@ -248,3 +248,44 @@ const shape = [
 for (let shapes of shape){
     shapes.duplicate()
 }
+
+//Mixins
+function mixin(target , ...sources){
+    Object.assign(target , ...sources)
+}
+
+const canEat = {
+    eat : function(){
+        this.hunger--;
+        console.log('Eating');
+    }
+}
+
+const canWalk = {
+    walk : function(){
+        console.log('Walking');
+    }
+}
+
+const canSwim = {
+    swim : function(){
+        console.log('Swimming');
+    }
+}
+
+function Person(){
+
+}
+mixin(Person.prototype,canEat ,canWalk);
+
+const person = new Person();
+console.log(person);
+
+
+function Fish(){
+
+}
+mixin(Fish.prototype,canEat ,canSwim);
+
+const fish = new Fish();
+console.log(fish);
